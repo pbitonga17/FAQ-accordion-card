63adf035c208f43f28df1a576277089ea1a3d9e6
PatriceBitonga <p.bitonga@gmail.com>
2022-06-28 17:31:13 -0600

    changes

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const questionLink = document.querySelectorAll('.h2');
 const arrowIcon = document.querySelectorAll('.arrow-icon');
-
+const h2 = document.querySelectorAll('.h2');
+const showAnswer1 = document.querySelectorAll('.hidden1');

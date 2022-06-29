9877a7f307372f4f00d9ecb7d8502c5fbaf24293
PatriceBitonga <p.bitonga@gmail.com>
2022-06-28 20:44:47 -0600

    js will be continue tmr

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,3 @@
 'use strict';
 
-const arrowBtn = document.querySelectorAll('.btn');
-const hiddenAnswer = document.querySelectorAll('.answer');
 
-let x;
-
-arrowBtn.forEach(arrowBtn => {
-    arrowBtn.addEventListener('click', () => {
-        hiddenAnswer.forEach(hiddenAnswer => {
-            hiddenAnswer.classList.remove('hidden');
-        })
-    })
-})

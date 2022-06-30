abd93072c434d3cd5ec9132480eb9347dd9f40a6
PatriceBitonga <p.bitonga@gmail.com>
2022-06-29 19:39:48 -0600

    added animation and js
    will continue tomorrow

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,47 @@
 'use strict';
 
+const questions = document.querySelectorAll('.question');
+const hiddenAnswer = document.querySelectorAll('.hidden');
+const arrowBtn = document.querySelectorAll('button');
+const h2Text = document.querySelectorAll('.h2');
+
+function showAnswer(button) {
+    hiddenAnswer.forEach(hiddenAnswer => {
+        hiddenAnswer.style.display = 'none';
+    })
+    
+    document.querySelector(`#${button}`).style.display = 'block';
+}
+
+
+arrowBtn.forEach(arrowBtn => {
+    arrowBtn.onclick = function() {
+        showAnswer(this.dataset.button);
+    }
+})
+
+
+
+
+/*arrowBtn.forEach(arrowBtn => {
+    arrowBtn.addEventListener('click', () => {
+        arrowBtn.style.transform = 'rotate(180deg)';
+        arrowBtn.style.transition = 'all .3s ease-out';
+        h2Text.forEach(h2Text => {
+            h2Text.style.fontWeight = '700';
+        })
+        
+        
+        arrowBtn.addEventListener('click', () => {
+            arrowBtn.style.transform = 'rotate(0 )';
+            arrowBtn.style.transition = 'all .3s ease-out';
+            h2Text.forEach(h2Text => {
+                h2Text.style.fontWeight = 'normal';
+            })
+        })
+    })
+})*/
+
+
+
 
